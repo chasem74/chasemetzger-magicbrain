57b84f5a888c09239673bc29ed16e9d35b938b8f
Chase Metzger <chasemetzger15@gmail.com>
2018-05-13 18:28:12 -0700

    Fixed the indentation (fucking stupid) and made the app logo a little better?

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import './App.css';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Logo />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Navigation />
+				<Logo />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,8 +8,8 @@ const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className='Tilt br2 shadow-2' options={{max: 55}} style={{width: 150, height: 150}}>
-				<div className='Tilt-inner pa3'>
-					<img style={{paddingTop: '5px', width: '100px', height: '100px'}} alt='brain logo' src={brain} />
+				<div className='Tilt-inner pa3' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}/*{paddingTop: '5px', width: '125px', height: '125px'}*/} >
+					<img alt='brain logo' src={brain} style={{width: '100px', height: '100px'}}/>
 				</div>
 			</Tilt>
 		</div>
