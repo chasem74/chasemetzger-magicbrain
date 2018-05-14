e58700748f6f7b6af89b434b8cb3cac6c5b2b1df
Chase Metzger <chasemetzger15@gmail.com>
2018-05-13 19:19:33 -0700

    Added cool particle effect on the background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -6,10 +8,24 @@ import Rank from './components/Rank/Rank';
 
 import './App.css';
 
+const particleOptions = {
+	particles: {
+		number:{
+			value: 200,
+		
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+};
+
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Particles className='particles' params={particleOptions} />
 				<Navigation />
 				<Logo />
 				<Rank />
